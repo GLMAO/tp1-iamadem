f1b73ab5d137c990867778c45c73ced6e1caf7c4
amiour <amiouradem3@gmail.com>
2025-11-02 22:28:17 +0300

    affichage de l heure a chaque seconde

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -1,6 +1,5 @@
 package org.emp.gl.core.launcher;
 
-import org.emp.gl.clients.CompteARebours;
 import org.emp.gl.clients.Horloge;
 import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
 import org.emp.gl.timer.service.TimerService;
@@ -10,7 +9,7 @@ public class App {
         testDuTimeService();
     }
 
-   /*  private static void testDuTimeService() {
+    private static void testDuTimeService() {
         // Instancier le TimerService
         TimerService timerService = new DummyTimeServiceImpl();
         
@@ -26,32 +25,7 @@ public class App {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }*/
-    private static void testDuTimeService() {
-    // Instancier le TimerService
-    TimerService timerService = new DummyTimeServiceImpl();
-    
-    // Instancier plusieurs horloges
-    Horloge horloge1 = new Horloge("Num 1", timerService);
-    Horloge horloge2 = new Horloge("Num 2", timerService);
-    
-    // Tester CompteARebours avec valeur 5
-    CompteARebours compte1 = new CompteARebours("Compte1", timerService, 5);
-    
-    // Instancier 10 CompteARebours avec valeurs aléatoires
-    java.util.Random random = new java.util.Random();
-    for (int i = 0; i < 7; i++) {
-        int valeurInitiale = 10 + random.nextInt(11); // 10-20
-        new CompteARebours("Compte-" + (i+2), timerService, valeurInitiale);
     }
-    
-    // Garder l'application en vie
-    try {
-        Thread.sleep(30000); // Attendre 30 secondes
-    } catch (InterruptedException e) {
-        e.printStackTrace();
-    }
-}
 
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
